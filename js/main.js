1ed86665326446bef28fd8e3446b24b04f8d4827
celenastefanie <celenastefanie@gmail.com>
2016-02-28 20:37:23 -0500

    finished last story

--- js/main.js
+++ js/main.js
@@ -28,3 +28,7 @@ if (story == 'a') {
 if (story == 'b') {
   document.write('Ameilia' + ' ' + (verb) + ' ' + 'through the' + ' ' + (adjective) + (colour) + ' ' + 'nebula escaping the space' + ' ' + (creature) + '.');
 }
+
+if (story == 'c') {
+  document.write('Jackson chose his' + ' ' + (adjective) + ',' + (colour) + ' ' + (creature) + 'card and' + ' ' + (verb) + ' ' + 'it to the table knowing he won.');
+}
