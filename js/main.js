53c35a0715df70e641e7427217cf776513020639
celenastefanie <celenastefanie@gmail.com>
2016-02-28 20:33:59 -0500

    made story b

--- js/main.js
+++ js/main.js
@@ -21,14 +21,10 @@ verb = prompt('A past tense verb (ran, burped, etc.)');
 
 story = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning\n Enter the letter below:');
 
-if (story = 'a') {
-  document.write('After snacking on' + ' ' + (adjective) + ' ' + 'treats, the' + ' ' + (colour) + ' ' + 'bellied' + ' ' + (creature) + ' ' + (verb) + ' ' + 'for hours.')
+if (story == 'a') {
+  document.write('After snacking on' + ' ' + (adjective) + ' ' + 'treats, the' + ' ' + (colour) + ' ' + 'bellied' + ' ' + (creature) + ' ' + (verb) + ' ' + 'for hours.');
 }
 
-if (story = 'b') {
-
-}
-
-if (story = 'c') {
-
+if (story == 'b') {
+  document.write('Ameilia' + ' ' + (verb) + ' ' + 'through the' + ' ' + (adjective) + (colour) + ' ' + 'nebula escaping the space' + ' ' + (creature) + '.');
 }
