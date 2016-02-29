3803d70fabbc94bbbfcc475dcc2a89e4cbdc4479
celenastefanie <celenastefanie@gmail.com>
2016-02-28 20:04:16 -0500

    added main code

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var madlib = alert('Let’s make a story together!');
+
+var lib1 = prompt('A colour (orange, indigo, etc.)');
+
+var lib2 = prompt('A creature (dragon, moth, etc.)');
+
+var lib3 = prompt('An adjective (beautiful, super, etc.)');
+
+var lib4 = prompt('Which story would you like?' + <br> + 'a) Snacking' + 'b) Escaping' + 'c) Winning' + 'Enter the letter below:');
